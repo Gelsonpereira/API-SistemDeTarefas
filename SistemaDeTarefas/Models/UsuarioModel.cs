6a988e67bc43853247561ded16571bdbf0a1b8f4
Gelson Junior <gelsonpicklis@hotmail.com>
2023-10-07 21:54:37 -0300

    SistemaDeTarefas- primeiro commit

---
+++ SistemaDeTarefas/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SistemaDeTarefas.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+    }
+}
